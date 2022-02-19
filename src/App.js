d155b6b651a1fe43154b363c1458a9550269dabe
LitvE <yevgen.litvinov1980@gmail.com>
2022-02-19 10:23:10 +0200

    jsx examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, { Fragment } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+// class / function
+
+
+//самый простой функциональный компонент
+/*function App() {
+  return null;
+}*/
+
+//самый простой классовый компонент
+/*
+class App extends React.Component {
+  render () {
+    return null;
+  };
+}*/
+
+class App extends React.Component {
+  render () {
+    return (
+      <Fragment>
+        <h1>sdfsdfsdfsdf</h1>
+        <p>10+2</p>
+        <p>{10+2}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {10+2} and some {2+2}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p id={2+1}>text1</p>
+        <p id={2+2}>text2</p>
+        <p id={2+3}>text3</p>
+      </Fragment>
+    );
+  };
 }
 
 export default App;
